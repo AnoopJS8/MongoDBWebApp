31e4532a9168fa8dc434a94769231cc01dc75124
AnoopJS8 <anoop.jyoti88@gmail.com>
2016-01-12 14:16:28 -0700

    Initializing Project

---
+++ src/main/java/com/mongodb/Blog/App.java
@@ -0,0 +1,13 @@
+package com.mongodb.Blog;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
