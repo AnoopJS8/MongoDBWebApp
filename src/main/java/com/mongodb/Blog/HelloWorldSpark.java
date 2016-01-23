6be370518add5eefefe551c42691c99abeea0d33
AnoopJS8 <anoop.jyoti88@gmail.com>
2016-01-23 15:01:10 -0700

    Added COntroller SessionDAO and UserDAO

--- src/main/java/com/mongodb/Blog/HelloWorldSpark.java
+++ src/main/java/com/mongodb/Blog/HelloWorldSpark.java
@@ -1,7 +1,8 @@
-package com.mongodb.Blog;
+/*package com.mongodb.Blog;
 import static spark.Spark.*;
 public class HelloWorldSpark {
 	public static void main(String[] args) {
 		get("/hello", (req, res) -> "Hello World");
 	}
 }
+*/
